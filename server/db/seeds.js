38eeea3925c2dd7976d9d0f7cea2c3d4ab8e67b3
John Taggart <taggartj1985@gmail.com>
2020-05-01 12:58:34 +0100

    added real seed data

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,12 +3,36 @@ db.dropDatabase();
 
 db.movies.insertMany([
 
-  {title: "test1",
-  genre: "test1",
-  rating: 15},
+  {title: "Raiders Of THe Lost Ark",
+  genre: "Action",
+  rating: "A",
+  release_date: "30th July 1981",
+  runtime: 115,
+  crew: [{director:"Steven Spielberg", writer:"George Lucas"}]},
 
-  {title: "test2",
-  genre: "test2",
-  rating: 15}
+  {title: "The Man From Earth",
+  genre: "Drama",
+  rating: "pg",
+  release_date: "10th June 2007",
+  runtime: 87,
+  crew: [{director:"Richard Schenkman", writer:"Jerome Bixby"}]},
+
+  {title: "A Tale Of Two Sisters",
+  genre: "Horror",
+  rating: "15",
+  release_date: "13th August 2004",
+  runtime: 114,
+  crew: [{director:"Jee-woon Kim", writer:"Jee-woon Kim"}]},
 
 ])
+
+
+//
+// Movies will be stored with the following data:
+//
+// Title
+// Genre
+// Rating (PG, 12A, 15, 18, etc)
+// Release Date (Stored as a string for now)
+// Runtime
+// Crew (An object storing director and producer details)
